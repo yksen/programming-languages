5cfcbc4d6228374c884aab9cb12dfb27d8f90082
xen <selegakamil@gmail.com>
2023-02-23 19:07:45 +0100

    Add glue_together, sum and product, test generators

--- list1/ex5.js
+++ list1/ex5.js
@@ -36,3 +36,57 @@ function print2(gen) {
     while (!(x = gen.next()).done)
         console.log(x.value);
 }
+
+function glue_together(gen, glue = ',') {
+    let result = "";
+    for (let x of gen) {
+        result += x + glue;
+    }
+    return result;
+}
+
+function sum(gen) {
+    let result = 0;
+    for (let x of gen) {
+        result += x;
+    }
+    return result;
+}
+
+function product(gen) {
+    let result = 1;
+    for (let x of gen) {
+        result *= x;
+    }
+    return result;
+}
+
+console.log("> Array.from");
+console.log(Array.from(divisors(10)));
+console.log(Array.from(primes(10)));
+console.log(Array.from(factorize(10)));
+
+console.log("> print");
+print(divisors(10));
+print(primes(10));
+print(factorize(10));
+
+console.log("> print2");
+print2(divisors(10));
+print2(primes(10));
+print2(factorize(10));
+
+console.log("> glue_together");
+console.log(glue_together(divisors(10)));
+console.log(glue_together(primes(10)));
+console.log(glue_together(factorize(10)));
+
+console.log("> sum");
+console.log(sum(divisors(10)));
+console.log(sum(primes(10)));
+console.log(sum(factorize(10)));
+
+console.log("> product");
+console.log(product(divisors(10)));
+console.log(product(primes(10)));
+console.log(product(factorize(10)));
