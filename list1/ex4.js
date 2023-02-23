edb344e350f69cfe97414eba798901df44e4be90
xen <selegakamil@gmail.com>
2023-02-23 18:35:37 +0100

    Check all getter values after different using different Circle setters

--- list1/ex4.js
+++ list1/ex4.js
@@ -6,27 +6,29 @@ class Square {
     set area(a) { this.a = Math.sqrt(a); }
     get circumference() { return 4 * this.a; }
     set circumference(l) { this.a = l / 4; }
-    toString() { return "a = ${ this.side } \nL = ${ this.circumference } \nP = ${ this.area } \n"; }
+    toString() { return `a = ${this.side}\nA = ${this.area}\nL = ${this.circumference}`; }
 }
 
 class Circle {
     constructor(r) { this.r = r; }
-    get radius() { return this.radius; }
-    set radius(r) { this.radius = r; }
     get diameter() { return 2 * this.r; }
     set diameter(d) { this.r = d / 2; }
     get area() { return Math.PI * this.r ** 2; }
     set area(a) { this.r = Math.sqrt(a / Math.PI); }
     get circumference() { return 2 * Math.PI * this.r; }
     set circumference(l) { this.r = l / (2 * Math.PI); }
-    toString() { return ""; }
+    toString() { return `r = ${this.r}\nA = ${this.area}\nL = ${this.circumference}`; }
 }
 
-let s = new Square(1);
-console.log(s);
-s.side = 12;
-console.log(s);
+function checkCircleGettersAndSetters(circle) {
+    circle.radius = 5;
+    console.log(circle.toString());
+    circle.diameter = 10;
+    console.log(circle.toString());
+    circle.area = 50;
+    console.log(circle.toString());
+    circle.circumference = 20;
+    console.log(circle.toString());
+}
 
-// var c = new Circle(4);
-// console.log(c);
-// c.radius = 6;
+checkCircleGettersAndSetters(new Circle(1));
