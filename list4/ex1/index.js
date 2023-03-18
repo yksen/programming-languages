8c71739c1ab2b487ca7ef9dc22ba328a12cb645c
xen <selegakamil@gmail.com>
2023-03-18 12:09:06 +0100

    Finish exercise 1

--- list4/ex1/index.js
+++ list4/ex1/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const { userInfo } = require("os");
 
 const PORT = 8080;
 
@@ -8,19 +9,39 @@ var paths = {
     "/a": "a.html",
     "/b": "b.html",
     "/c": "c.html",
+    "/image.png": "image.png",
+    "/jpqui-lista4-2023.pdf": "../jpqui-lista4-2023.pdf",
 };
+
+var useReadFileSync = false;
 
 http.createServer((request, response) => {
     let path = paths[request.url];
     if (path == undefined) {
         response.writeHead(404, { "Content-Type": "text/html" });
-        response.write("404 Not Found");
+    } else if (path.endsWith(".pdf")) {
+        response.writeHead(200, { "Content-Type": "application/pdf" });
+    } else if (path.endsWith(".png")) {
+        response.writeHead(200, { "Content-Type": "image/png" });
     } else {
-        let html = fs.readFileSync(path);
         response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(html);
     }
-    response.end();
+
+    let content = "<h1>404 Not Found</h1>";
+    if (useReadFileSync) {
+        if (path != undefined)
+            content = fs.readFileSync(path);
+        response.end(content);
+    } else {
+        if (path == undefined)
+            response.end(content);
+        else
+            fs.readFile(path, (error, data) => {
+                if (error) throw error;
+                response.end(data);
+            });
+    }
+
 }).listen(PORT);
 
 console.log(`Listening on port ${PORT}`);
