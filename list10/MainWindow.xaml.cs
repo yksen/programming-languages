16d2bebf3d590bb0d2d31e9e454281d17dca47f8
xen <selegakamil@gmail.com>
2023-05-25 13:43:27 +0200

    Draw graph vertices

--- list10/MainWindow.xaml.cs
+++ list10/MainWindow.xaml.cs
@@ -15,11 +15,49 @@ using System.Windows.Shapes;
 
 namespace list10
 {
+    // <summary>
+    // Interaction logic for MainWindow.xaml
+    // </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            drawGraph();
+        }
+
+        private void drawGraph()
+        {
+            uint n = 10;
+            double r = 100;
+            double x0 = 200;
+            double y0 = 200;
+            double alpha = 2 * Math.PI / n;
+            double x, y;
+
+            for (uint i = 0; i < n; i++)
+            {
+                x = x0 + r * Math.Cos(i * alpha);
+                y = y0 + r * Math.Sin(i * alpha);
+                drawVertex(i + 1, x, y);
+            }
+        }
+
+        private void drawVertex(uint number, double x, double y)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Width = 50;
+            ellipse.Height = 50;
+            ellipse.Fill = Brushes.Red;
+            ellipse.Stroke = Brushes.Black;
+            ellipse.StrokeThickness = 2;
+            ellipse.MouseDown += Canvas_MouseLeftButtonDown;
+            ellipse.MouseUp += Canvas_MouseLeftButtonUp;
+            ellipse.MouseRightButtonDown += Canvas_MouseRightButtonDown;
+            ellipse.MouseRightButtonUp += Canvas_MouseRightButtonUp;
+            Canvas.SetLeft(ellipse, x);
+            Canvas.SetTop(ellipse, y);
+            canvas.Children.Add(ellipse);
         }
 
         private void AddEdgeButton_Click(object sender, RoutedEventArgs e)
